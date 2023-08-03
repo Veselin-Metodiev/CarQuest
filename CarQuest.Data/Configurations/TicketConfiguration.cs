32719669b323f9285be7c3ff917ee319b190fa87
Veselin-Metodiev <vesselin.metodiev@gmail.com>
2023-08-03 23:27:01 +0300

    Resolved circular dependency

---
+++ CarQuest.Data/Configurations/TicketConfiguration.cs
@@ -0,0 +1,19 @@
+﻿namespace CarQuest.Data.Configurations;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using Models;
+
+public class TicketConfiguration : IEntityTypeConfiguration<Ticket>
+{
+	public void Configure(EntityTypeBuilder<Ticket> builder)
+	{
+		builder
+			.HasOne(t => t.Car)
+			.WithMany(c => c.Tickets)
+			.HasForeignKey(t => t.CarId)
+			.OnDelete(DeleteBehavior.NoAction)
+			.HasConstraintName("FK_Ticket_Cars_CarId");
+	}
+}
