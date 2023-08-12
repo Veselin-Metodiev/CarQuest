82bbff37d1d393b041cd11b22b71259a59bfc90e
Veselin-Metodiev <vesselin.metodiev@gmail.com>
2023-08-12 14:59:51 +0300

    refactoring

--- CarQuest.Data/CarQuestDbContext.cs
+++ CarQuest.Data/CarQuestDbContext.cs
@@ -23,6 +23,22 @@ public class CarQuestDbContext : IdentityDbContext<ApplicationUser, IdentityRole
     {
         builder.ApplyConfiguration(new TicketConfiguration());
 
+        builder.Entity<ApplicationUser>().HasData(
+	        new ApplicationUser
+	        {
+		        UserName = "Admin",
+		        NormalizedUserName = "ADMIN",
+		        Email = "admin@carquest.com",
+		        NormalizedEmail = "ADMIN@CARQUEST.COM",
+		        EmailConfirmed = true,
+		        PasswordHash = "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92",
+		        ConcurrencyStamp = "8b51706e-f6e8-4dae-b240-54f856fb3004",
+		        SecurityStamp = "f6af46f5-74ba-43dc-927b-ad83497d0387",
+		        TwoFactorEnabled = false,
+		        FirstName = "Admin",
+		        LastName = "Admin"
+	        });
+
         base.OnModelCreating(builder);
     }
 }
